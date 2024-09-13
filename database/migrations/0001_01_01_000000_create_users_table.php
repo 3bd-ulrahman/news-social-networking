e164bc2acbe6a7b28e95d3a41c4650c448302bf3
3bd-ulrahman <abdulrahman198276301@gmail.com>
2024-09-13 20:54:03 +0300

    database migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username', 50)->unique();
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone', 20)->unique();
+            $table->string('avatar');
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->string('street')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
